e049ea5d80de82f90643b72d56eee7410d015e56
Dong Han <lux00leo@gmail.com>
2023-12-27 09:25:17 +0900

    tutorial finished

--- src/main/java/partyquest/chat/tutorial/TutorialController.java
+++ src/main/java/partyquest/chat/tutorial/TutorialController.java
@@ -4,6 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
 @Controller
@@ -11,7 +13,13 @@ public class TutorialController {
     @MessageMapping("/hello")
     @SendTo("/topic/tutorial") // '/topic/tutorial'채널에 응답한다.
     public Greeting greeting(HelloMessage message) throws InterruptedException {
-        Thread.sleep(1000);
+        //Thread.sleep(1000);
         return new Greeting(String.format("Hello %s", message.getName()));
+    }
+
+    @RequestMapping(value = "/tutorial")
+    @GetMapping
+    public String index() {
+        return "tutorial/index";
     }
 }
